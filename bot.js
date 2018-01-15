e5fec1188f9f7e447fef948780685eaed3503a01
Light Yagami <tonybob2609@gmail.com>
2018-01-14 16:25:26 -0800

    Update bot.js
    Added !purge

--- bot.js
+++ bot.js
@@ -10,6 +10,12 @@ bot.on("messageCreate", (msg) => {
 bot.on("messageCreate", (msg) => {
     if(msg.content === "!help") {
         bot.createMessage(msg.channel.id, "**Commands:** \n **Prefix:** !\n **Ping:** Pong. \n **Help:** Displays this message with all the commands of the bot."
-                          }
-                          });
+    }
+});
+bot.on("messageCreate", (msg) => {
+    var args = message.content.split(" ")
+    if(args[0] === "!purge") {
+        bot.purgeChannel(msg.channel.id, args[1]);
+    }
+});
 bot.connect();
