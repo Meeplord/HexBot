d1dda2f778c4546c199fe34e9681a92934d98cc2
Light Yagami <tonybob2609@gmail.com>
2018-01-14 16:23:00 -0800

    Update bot.js
    Added !help

--- bot.js
+++ bot.js
@@ -7,4 +7,9 @@ bot.on("messageCreate", (msg) => {
         bot.createMessage(msg.channel.id, "Pong!");
     }
 });
+bot.on("messageCreate", (msg) => {
+    if(msg.content === "!help") {
+        bot.createMessage(msg.channel.id, "**Commands:** \n **Prefix:** !\n **Ping:** Pong. \n **Help:** Displays this message with all the commands of the bot."
+                          }
+                          });
 bot.connect();
